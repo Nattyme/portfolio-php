4027f99d5339521707faba4a46ee00f7595b8c19
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-07-15 19:15:05 +0300

    config.php: protocol check

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
+
+if( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+  $protocol = 'https://';
+} else {
+  $protocol = 'http://';
+}
 // Хост сайта - используется для ссылок в браузере < href="..">
-define('HOST', '//' . $_SERVER['HTTP_HOST'] . '/'); //  //project/
+define('HOST', $protocol . $_SERVER['HTTP_HOST'] . '/'); //  //project/
 
 //Физический путь к корневой директории скрипта
 define('ROOT', dirname(__FILE__) . '/');
