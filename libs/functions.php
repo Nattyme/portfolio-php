9198b132dba04a7ac999951d723028e479218c07
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-07-24 14:38:55 +0300

    functions: date and time with translate to rus lang

--- libs/functions.php
+++ libs/functions.php
@@ -44,3 +44,66 @@ function getUriGet () {
   // Запись выше можно сделать короче через тернарный оператор
   // $uriGet = isset($uri[1]) ? $uri[1] : null;
 }
+
+function rus_date () {
+  // Перевод
+  $translate = array(
+		"am" => "дп",
+		"pm" => "пп",
+		"AM" => "ДП",
+		"PM" => "ПП",
+		"Monday" => "Понедельник",
+		"Mon" => "Пн",
+		"Tuesday" => "Вторник",
+		"Tue" => "Вт",
+		"Wednesday" => "Среда",
+		"Wed" => "Ср",
+		"Thursday" => "Четверг",
+		"Thu" => "Чт",
+		"Friday" => "Пятница",
+		"Fri" => "Пт",
+		"Saturday" => "Суббота",
+		"Sat" => "Сб",
+		"Sunday" => "Воскресенье",
+		"Sun" => "Вс",
+		"January" => "Января",
+		"Jan" => "Янв",
+		"February" => "Февраля",
+		"Feb" => "Фев",
+		"March" => "Марта",
+		"Mar" => "Мар",
+		"April" => "Апреля",
+		"Apr" => "Апр",
+		"May" => "Мая",
+		"May" => "Мая",
+		"June" => "Июня",
+		"Jun" => "Июн",
+		"July" => "Июля",
+		"Jul" => "Июл",
+		"August" => "Августа",
+		"Aug" => "Авг",
+		"September" => "Сентября",
+		"Sep" => "Сен",
+		"October" => "Октября",
+		"Oct" => "Окт",
+		"November" => "Ноября",
+		"Nov" => "Ноя",
+		"December" => "Декабря",
+		"Dec" => "Дек",
+		"st" => "ое",
+		"nd" => "ое",
+		"rd" => "е",
+		"th" => "ое"
+  );
+  // если передали дату, то переводим её
+  if ( func_num_args() > 1 ) {
+      return strtr(date(func_get_arg(0), func_get_arg(1)), $translate);
+  } 
+  // Иначе генерируем текущее время по шаблону
+  else {
+      return strtr(date(func_get_arg(0)), $translate);
+  }
+}
+
+echo rus_date("j l F Y H:i", time());
+die();
