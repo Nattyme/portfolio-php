2146046ea7de9801a3449892aff45892fe5516f2
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-07-25 15:43:52 +0300

    admin: download img to post content using ckeditor

--- libs/ck-upload/upload.php
+++ libs/ck-upload/upload.php
@@ -1,13 +1,14 @@
 <?php
-require_once "./../../config.php";
+require "./../../config.php";
 
 // Required: anonymous function reference number as explained above.
 $funcNum = $_GET['CKEditorFuncNum'] ;
 
 // Check the $_FILES array and save the file. Assign the correct path to a variable ($url).
-$url = HOST . 'usercontent/editor-uploads/img.png';
+require "./image-upload.php";
 
 // Usually you will only assign something here if the file could not be uploaded.
-$message = 'The uploaded file has been renamed';
+// $message = 'The uploaded file has been renamed';
 
+$message = 'Файл загружен';
 echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction($funcNum, '$url', '$message');</script>";
