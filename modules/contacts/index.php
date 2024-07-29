1b1fa8992d54a74a1d98dc24e51a10c8e328d75a
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-07-30 01:14:19 +0300

    public: contacts base files

--- modules/contacts/index.php
+++ modules/contacts/index.php
@@ -1,4 +1,9 @@
 <?php 
-  include ROOT . "templates/_parts/_header.tpl";
-  // include ROOT . "templates/main/main.tpl";
-  include ROOT . "templates/_parts/_footer.tpl";
+  $pageTitle = "Контакты";
+  include ROOT . 'templates/page-parts/_head.tpl';
+  include ROOT . 'templates/_parts/_header.tpl';
+
+  include ROOT . 'templates/contacts/contacts.tpl';
+
+  include ROOT . 'templates/_parts/_footer.tpl';
+  include ROOT . 'templates/page-parts/_foot.tpl';
--- modules/profile/profile.php
+++ modules/profile/profile.php
@@ -33,6 +33,8 @@
 
   include ROOT . 'templates/page-parts/_head.tpl';
   include ROOT . 'templates/_parts/_header.tpl';
+
   include ROOT . 'templates/profile/profile.tpl';
+  
   include ROOT . 'templates/_parts/_footer.tpl';
   include ROOT . 'templates/page-parts/_foot.tpl';
