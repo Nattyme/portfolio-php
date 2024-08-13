1a92b0f94a3a69a4931b3204620bb1089eb1c8c7
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-13 23:25:05 +0300

    public: logged user add one product to cart

--- modules/cart/addtocart.php
+++ modules/cart/addtocart.php
@@ -1,1 +1,5 @@
-<h1>Корзина</h1>
+<?php
+$user = R::load('users', $_SESSION['logged_user']['id']);
+$user->cart = $_GET['id'];
+R::store($user);
+header('Location: ' . HOST . 'shop/' . $_GET['id']);
