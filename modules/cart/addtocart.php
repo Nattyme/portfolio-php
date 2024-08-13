1d73ea44ebda46629f38dfa1a807bc753584a5d8
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-13 23:14:21 +0300

    public: shop product card link style correction

--- modules/cart/addtocart.php
+++ modules/cart/addtocart.php
@@ -1,6 +1,1 @@
-<?php
-$user = R::load('users', $_SESSION['logged_user']['id']);
-$user->cart = $_GET['id'];
-R::store($user);
-header("Location: " . HOST . 'shop/' . $_GET['id']);
-exit();
+<h1>Корзина</h1>
