76ce2768927ce2cf781804fc22b9546e410a8aca
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-14 20:55:49 +0300

    public: one product added tto cart saved to db at json

--- modules/cart/addtocart.php
+++ modules/cart/addtocart.php
@@ -1,5 +1,13 @@
 <?php
+// Находим пользователя в БД по id
 $user = R::load('users', $_SESSION['logged_user']['id']);
-$user->cart = $_GET['id'];
+
+// Формируем корзину в ассоциативный массив
+$cart = [$_GET['id'] => 1];
+
+// Превращаем корзину в json строку
+$user->cart = json_encode($cart);
+
+// Обноваляем пользователя в БД
 R::store($user);
 header('Location: ' . HOST . 'shop/' . $_GET['id']);
