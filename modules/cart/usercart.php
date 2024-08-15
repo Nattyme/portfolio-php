0dfa3cd9e897d8ed6523e9aee2c7be3fb403de25
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-15 23:21:16 +0300

    public: count logic for unlogged user cart

--- modules/cart/usercart.php
+++ modules/cart/usercart.php
@@ -1,6 +1,8 @@
 <?php
+$cartCount = 0;
+
 if( isset($_SESSION['cart']) ) {
   $cartCount = array_sum($_SESSION['cart']);
-} else {
-  $cartCount = 0;
+} elseif ( isset($_COOKIE['cart']) && !empty($_COOKIE['cart']) ) {
+  $cartCount = array_sum(json_decode($_COOKIE['cart'], true));
 }
