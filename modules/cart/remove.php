84bfa745bd9e892509de99c46c75b15c27a04a16
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-16 15:24:05 +0300

    public: remove products from unlogged user cart

--- modules/cart/remove.php
+++ modules/cart/remove.php
@@ -1,24 +1,44 @@
 <?php
-// Находим пользователя в БД по id
-$user = R::load('users', $_SESSION['logged_user']['id']);
+if ( isLoggedIn() ) {
+  // Находим пользователя в БД по id
+  $user = R::load('users', $_SESSION['logged_user']['id']);
 
-// Получаем корзину из БД
-$cart = json_decode($user->cart, true);
+  // Получаем корзину из БД
+  $cart = json_decode($user->cart, true);
 
-// Удаляем товар из корзины
-unset($cart[$_GET['id']]);
+  // Удаляем товар из корзины
+  unset($cart[$_GET['id']]);
 
-// Превращаем корзину в json строку
-$user->cart = json_encode($cart);
+  // Превращаем корзину в json строку
+  $user->cart = json_encode($cart);
 
-// Обноваляем пользователя в БД
-R::store($user);
+  // Обноваляем пользователя в БД
+  R::store($user);
 
-// Обновляем состояние корзины в сессии
-$_SESSION['cart'] = $cart;
+  // Обновляем состояние корзины в сессии
+  $_SESSION['cart'] = $cart;
 
-// Сообщение об удалении товара
-$_SESSION['success'][] = ['title' => 'Товар был удалён из корзины.'];
+  // Сообщение об удалении товара
+  $_SESSION['success'][] = ['title' => 'Товар был удалён из корзины.'];
+}
+
+if ( !isLoggedIn() ) {
+  if (isset($_COOKIE['cart'])) {
+    // Получаем корзину из COOKIE
+    $cart = json_decode($_COOKIE['cart'], true);
+  } else {
+    $cart = array();
+  }
+
+  // 3. Удаляем товар из корзины
+  unset($cart[$_GET['id']]);
+
+  // 4. Сохранение корзины в COOKIE
+  setcookie('cart', json_encode($cart), time() + 60 * 60 * 24 * 30);
+
+  // Сообщение об удалении товара
+  $_SESSION['success'][] = ['title' => 'Товар был удалён из корзины.'];
+}
 
 header('Location: ' . HOST . 'cart');
 exit();
--- modules/cart/usercart.php
+++ modules/cart/usercart.php
@@ -1,10 +1,10 @@
 <?php
 
 // Определяем корзину
-$cart = 0;
-if ( isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
-  $cart = json_decode($_SESSION['cart'], true);
-} elseif ( isset($_COOKIE['cart']) && !empty($_COOKIE['cart']) ) {
+$cart = array();
+if ( isLoggedIn() && isset($_SESSION['cart'])) {
+  $cart = $_SESSION['cart'];
+} else if ( isset($_COOKIE['cart']) && !empty($_COOKIE['cart']) ) {
   $cart = json_decode($_COOKIE['cart'], true);
 }
 
