be8b6b3d212b6b633a24a29b33d4d3b752dd7c92
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-08-17 14:04:16 +0300

    public: order form data save to db logic

--- modules/orders/new.php
+++ modules/orders/new.php
@@ -1,4 +1,46 @@
 <?php 
+if (isset($_POST['submit'])) {
+  // Проверка если поля пустые
+  if  ( empty(trim($_POST['name'])) ) {
+    $_SESSION['errors'][] = ['title' => 'Поле "имя" мустое. Заполните данные для отправки.'];
+  } 
+
+  if ( empty(trim($_POST['surname'])) ) {
+    $_SESSION['errors'][] = ['title' => 'Поле "фамилия" мустое. Заполните данные для отправки.'];
+  }
+
+  if ( empty(trim($_POST['email'])) ) {
+    $_SESSION['errors'][] = ['title' => 'Поле "email" мустое. Заполните данные для отправки.'];
+  }
+
+  if ( empty(trim($_POST['phone'])) ) {
+    $_SESSION['errors'][] = ['title' => 'Поле "телефон" мустое. Заполните данные для отправки.'];
+  }
+
+  if ( empty(trim($_POST['address'])) ) {
+    $_SESSION['errors'][] = ['title' => 'Поле "адрес" мустое. Заполните данные для отправки.'];
+  } 
+
+  if ( empty($_SESSION['errors'])) {
+    $order = R::dispense('orders');
+
+    $order->name = htmlentities(trim($_POST['name']));
+    $order->surname = htmlentities(trim($_POST['surname']));
+
+    $order->email = filter_var(htmlentities(trim($_POST['email'])), FILTER_VALIDATE_EMAIL);
+    $order->phone = trim($_POST['phone']);
+
+    $order->address = htmlentities(trim($_POST['message']));
+
+    $order->cart = json_encode($cart);
+
+    R::store($order);
+    header('Location: ' . HOST . 'ordercreated');
+    exit();
+
+  }
+}
+
 if ( !empty($cart) ) {
 
   $products = R::findLike ('products', ['id' => array_keys($cart)]); 
