4d71dab935896237b5483687f842d0749c8b6469
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-09-05 19:49:41 +0300

    admin: post edit and post new pages display categories of section blog to choose

--- modules/login/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_destroy();
-setcookie(session_name(), '', time() - 60);
-
-header("Location: " . HOST);
--- modules/navigation/navigation.php
+++
@@ -1,3 +0,0 @@
-<?php
-require ROOT . 'modules/navigation/sub-nav/_shop.php';
-require ROOT . 'modules/navigation/sub-nav/_blog.php';
