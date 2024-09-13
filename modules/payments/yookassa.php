1479066cb5ca2246fb33e33f563bc62ae95d2f34
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-09-13 16:46:16 +0300

    public: yookassa test new payment

--- modules/payments/yookassa.php
+++ modules/payments/yookassa.php
@@ -10,9 +10,29 @@ $client->setAuth(SHOP_ID, SECRET_KEY);
 
 // Получение информации о магазине
 try {
-    $response = $client->me();
+  $payment = $client->createPayment(
+    array(
+        'amount' => array(
+            'value' => 100.0,
+            'currency' => 'RUB',
+        ),
+        'confirmation' => array(
+            'type' => 'redirect',
+            'return_url' => HOST . 'shop',
+        ),
+        'capture' => true,
+        'description' => 'Заказ №1',
+    ),
+    uniqid('', true)
+  );
+
+  // Получаем confirmation url
+  $confirmationUrl = $payment->getConfirmation()->getConfirmationUrl();
 } catch (\Exception $e) {
     $response = $e;
 }
 
-var_dump($response);
+// var_dump($payment);
+// Перенаправляем пользователя на форму оплаты
+header('Location: ' .  $confirmationUrl);
+exit();
